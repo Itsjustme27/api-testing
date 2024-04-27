919bbaa1a8753618c58afbd95bdaf603300acced
Itsjustme27 <reachprayush@gmail.com>
2024-04-27 20:29:37 +0545

    added data

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let div = document.getElementById("data");
+
 
 fetch("http://localhost:3000/softwares")
     .then(response => response.json())
     .then(data => {
-        const software = data[0]; // Accessing the first software object in the array
-        div.innerHTML = `<h1>${software.id}</h1>
-                        <p>${software.name}</p>
-                        <p>${software.description}</p>
-                        <a href="${software.link}"><button>Click to Download</button></a>`;
+        const container = document.querySelector("#container");
+        data.forEach(software => {
+            const div = document.createElement('div');
+            div.classList.add('data', 'justify-content-center', 'm-3', 'rounded-5');
+            div.innerHTML = `<img src="${software.img}"/>
+                            <p>${software.name}</p>
+                            <p>${software.description}</p>
+                            <a href="${software.link}"><button class="btn btn-outline-dark w-100">Click to Download</button></a>`;
+            container.appendChild(div);
+        });
     })
     .catch(error => {
         console.error('Error fetching data:', error);
